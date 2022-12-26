543900763fcaf6277d9af7b426ea0a219909755b
revolt144 <74239459+revolt144@users.noreply.github.com>
2022-12-26 17:03:12 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'Revolt' 
+global.apikey = ["rev", "Rico", "Revolt", "Rev", "Rev-Bot"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
